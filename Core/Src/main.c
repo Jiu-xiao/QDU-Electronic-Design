08f3a0a4d82a6a953aed7f6722f6b1c0ade5b41f
“yJiu-xiao <2592509183@qq.com>
2020-11-24 08:29:34 +0800

    串口输出测试

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,24 +1,25 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under Ultimate Liberty license
+ * SLA0044, the "License"; You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *                             www.st.com/SLA0044
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
 #include "cmsis_os.h"
 #include "stdio.h"
 
@@ -49,45 +50,39 @@ UART_HandleTypeDef huart1;
 /* Definitions for Task_center */
 osThreadId_t Task_centerHandle;
 const osThreadAttr_t Task_center_attributes = {
-  .name = "Task_center",
-  .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 128 * 4
-};
+    .name = "Task_center",
+    .priority = (osPriority_t)osPriorityNormal,
+    .stack_size = 128 * 4};
 /* Definitions for Task_key_scan */
 osThreadId_t Task_key_scanHandle;
 const osThreadAttr_t Task_key_scan_attributes = {
-  .name = "Task_key_scan",
-  .priority = (osPriority_t) osPriorityLow,
-  .stack_size = 128 * 4
-};
+    .name = "Task_key_scan",
+    .priority = (osPriority_t)osPriorityLow,
+    .stack_size = 128 * 4};
 /* Definitions for Task_NTC_scan */
 osThreadId_t Task_NTC_scanHandle;
 const osThreadAttr_t Task_NTC_scan_attributes = {
-  .name = "Task_NTC_scan",
-  .priority = (osPriority_t) osPriorityLow,
-  .stack_size = 128 * 4
-};
+    .name = "Task_NTC_scan",
+    .priority = (osPriority_t)osPriorityLow,
+    .stack_size = 128 * 4};
 /* Definitions for Task_led */
 osThreadId_t Task_ledHandle;
 const osThreadAttr_t Task_led_attributes = {
-  .name = "Task_led",
-  .priority = (osPriority_t) osPriorityLow,
-  .stack_size = 128 * 4
-};
+    .name = "Task_led",
+    .priority = (osPriority_t)osPriorityLow,
+    .stack_size = 128 * 4};
 /* Definitions for Task_display */
 osThreadId_t Task_displayHandle;
 const osThreadAttr_t Task_display_attributes = {
-  .name = "Task_display",
-  .priority = (osPriority_t) osPriorityLow,
-  .stack_size = 128 * 4
-};
+    .name = "Task_display",
+    .priority = (osPriority_t)osPriorityLow,
+    .stack_size = 128 * 4};
 /* Definitions for Task_output */
 osThreadId_t Task_outputHandle;
 const osThreadAttr_t Task_output_attributes = {
-  .name = "Task_output",
-  .priority = (osPriority_t) osPriorityLow,
-  .stack_size = 128 * 4
-};
+    .name = "Task_output",
+    .priority = (osPriority_t)osPriorityLow,
+    .stack_size = 128 * 4};
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -114,24 +109,23 @@ void Task_output_f(void *argument);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int fputc(int ch,FILE *f)
-{
-    uint8_t temp[1]={ch};
-    HAL_UART_Transmit(&huart1,temp,1,2);
-    return 0;
+ * @brief  The application entry point.
+ * @retval int
+ */
+int fputc(int ch, FILE *f) {
+  uint8_t temp[1] = {ch};
+  HAL_UART_Transmit(&huart1, temp, 1, 2);
+  return 0;
 }
-int main(void)
-{
+int main(void) {
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -177,16 +171,19 @@ int main(void)
   Task_centerHandle = osThreadNew(Task_center_f, NULL, &Task_center_attributes);
 
   /* creation of Task_key_scan */
-  Task_key_scanHandle = osThreadNew(Task_key_scan_f, NULL, &Task_key_scan_attributes);
+  Task_key_scanHandle =
+      osThreadNew(Task_key_scan_f, NULL, &Task_key_scan_attributes);
 
   /* creation of Task_NTC_scan */
-  Task_NTC_scanHandle = osThreadNew(Task_NTC_scan_f, NULL, &Task_NTC_scan_attributes);
+  Task_NTC_scanHandle =
+      osThreadNew(Task_NTC_scan_f, NULL, &Task_NTC_scan_attributes);
 
   /* creation of Task_led */
   Task_ledHandle = osThreadNew(Task_led_f, NULL, &Task_led_attributes);
 
   /* creation of Task_display */
-  Task_displayHandle = osThreadNew(Task_display_f, NULL, &Task_display_attributes);
+  Task_displayHandle =
+      osThreadNew(Task_display_f, NULL, &Task_display_attributes);
 
   /* creation of Task_output */
   Task_outputHandle = osThreadNew(Task_output_f, NULL, &Task_output_attributes);
@@ -201,8 +198,7 @@ int main(void)
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -211,17 +207,16 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -229,33 +224,29 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM2_Init(void)
-{
-
+ * @brief TIM2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_TIM2_Init(void) {
   /* USER CODE BEGIN TIM2_Init 0 */
 
   /* USER CODE END TIM2_Init 0 */
@@ -272,39 +263,33 @@ static void MX_TIM2_Init(void)
   htim2.Init.Period = 65535;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
-  {
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK) {
     Error_Handler();
   }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK) {
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
   sConfigOC.Pulse = 0;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
-  {
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_4) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
   HAL_TIM_MspPostInit(&htim2);
-
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART1_UART_Init(void)
-{
-
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
   /* USER CODE BEGIN USART1_Init 0 */
 
   /* USER CODE END USART1_Init 0 */
@@ -320,23 +305,20 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.Mode = UART_MODE_TX_RX;
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
+  if (HAL_UART_Init(&huart1) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN USART1_Init 2 */
 
   /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
@@ -349,12 +331,15 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13
-                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
-                          |GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB,
+                    GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 |
+                        GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
+                        GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
+                    GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
+                    GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PA0 */
   GPIO_InitStruct.Pin = GPIO_PIN_0;
@@ -364,7 +349,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB0 PB1 PB2 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2;
+  GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -372,9 +357,9 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pins : PB10 PB11 PB12 PB13
                            PB3 PB4 PB5 PB6
                            PB7 PB8 PB9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13
-                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
-                          |GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 |
+                        GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
+                        GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -387,12 +372,11 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PC7 PC8 PC9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Pin = GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
 }
 
 /* USER CODE BEGIN 4 */
@@ -401,124 +385,145 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN Header_Task_center_f */
 /**
-  * @brief  Function implementing the Task_center thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the Task_center thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_Task_center_f */
-void Task_center_f(void *argument)
-{
+void Task_center_f(void *argument) {
   /* USER CODE BEGIN 5 */
-  printf("center initing..\n");
+  osKernelLock();
+  printf("center initing..\r\n");
+  osKernelUnlock();
   /* Infinite loop */
-  while(1){
-    ;
+  while (1) {
+    osDelay(1000);
+    osKernelLock();
+    printf("center is here\r\n");
+    osKernelUnlock();
   }
   /* USER CODE END 5 */
 }
 
 /* USER CODE BEGIN Header_Task_key_scan_f */
 /**
-* @brief Function implementing the Task_key_scan thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the Task_key_scan thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_Task_key_scan_f */
-void Task_key_scan_f(void *argument)
-{
+void Task_key_scan_f(void *argument) {
   /* USER CODE BEGIN Task_key_scan_f */
-  printf("key scan initing..\n");
+  osKernelLock();
+  printf("key scan initing..\r\n");
+  osKernelUnlock();
   /* Infinite loop */
-  while(1){
-    ;
+  while (1) {
+    osDelay(1000);
+    osKernelLock();
+    printf("key scan is here\r\n");
+    osKernelUnlock();
   }
   /* USER CODE END Task_key_scan_f */
 }
 
 /* USER CODE BEGIN Header_Task_NTC_scan_f */
 /**
-* @brief Function implementing the Task_NTC_scan thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the Task_NTC_scan thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_Task_NTC_scan_f */
-void Task_NTC_scan_f(void *argument)
-{
+void Task_NTC_scan_f(void *argument) {
   /* USER CODE BEGIN Task_NTC_scan_f */
-  printf("NTC scan initing..\n");
+  osKernelLock();
+  printf("NTC scan initing..\r\n");
+  osKernelUnlock();
   /* Infinite loop */
-  while(1){
-    ;
+  while (1) {
+    osDelay(1000);
+    osKernelLock();
+    printf("NTC scan is here\r\n");
+    osKernelUnlock();
   }
   /* USER CODE END Task_NTC_scan_f */
 }
 
 /* USER CODE BEGIN Header_Task_led_f */
 /**
-* @brief Function implementing the Task_led thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the Task_led thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_Task_led_f */
-void Task_led_f(void *argument)
-{
+void Task_led_f(void *argument) {
   /* USER CODE BEGIN Task_led_f */
+  osKernelLock();
+  printf("led initing..\r\n");
+  osKernelUnlock();
   /* Infinite loop */
-  printf("led initing..\n");
-  /* Infinite loop */
-  while(1){
-    ;
+  while (1) {
+    osDelay(1000);
+    osKernelLock();
+    printf("led is here\r\n");
+    osKernelUnlock();
   }
   /* USER CODE END Task_led_f */
 }
 
 /* USER CODE BEGIN Header_Task_display_f */
 /**
-* @brief Function implementing the Task_display thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the Task_display thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_Task_display_f */
-void Task_display_f(void *argument)
-{
+void Task_display_f(void *argument) {
   /* USER CODE BEGIN Task_display_f */
-  printf("display initing..\n");
+  osKernelLock();
+  printf("display initing..\r\n");
+  osKernelUnlock();
   /* Infinite loop */
-  while(1){
-    ;
+  while (1) {
+    osDelay(1000);
+    osKernelLock();
+    printf("display is here\r\n");
+    osKernelUnlock();
   }
   /* USER CODE END Task_display_f */
 }
 
 /* USER CODE BEGIN Header_Task_output_f */
 /**
-* @brief Function implementing the Task_output thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the Task_output thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_Task_output_f */
-void Task_output_f(void *argument)
-{
+void Task_output_f(void *argument) {
   /* USER CODE BEGIN Task_output_f */
+  osKernelLock();
+  printf("output initing..\r\n");
+  osKernelUnlock();
   /* Infinite loop */
-  printf("output initing..\n");
-  /* Infinite loop */
-  while(1){
-    ;
+  while (1) {
+    osDelay(1000);
+    osKernelLock();
+    printf("output is here\r\n");
+    osKernelUnlock();
   }
   /* USER CODE END Task_output_f */
 }
 
 /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM1 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM1 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   /* USER CODE BEGIN Callback 0 */
 
   /* USER CODE END Callback 0 */
@@ -531,30 +536,30 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
 
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line)
+   */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
